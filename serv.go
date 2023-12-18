e8c5dd097f268adb52a704dc63a7896672cfc8f2
coquentin <quentin.contreau@ynov.com>
2023-12-18 11:29:09 +0100

    advenced

---
+++ serv.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	template.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+}
